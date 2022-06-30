78d6e8a843f996fcfe0eeb2b6f0c99add72e1fc0
Adriano Santos <adriradiologista@hotmail.com.br>
2022-06-30 18:05:51 -0300

    Melhorando e corrigindo alguns BUGs de uma matriz.

--- matriz/src/Main.java
+++ matriz/src/Main.java
@@ -7,24 +7,25 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Construindo uma matriz:");
-        System.out.print("Informe o número de linhas: ");
+        System.out.println("Entre com os seguintes dados:");
+        System.out.print("Nª de linhas: "); // Informando o número de linhas de uma matriz.
         int n = scanner.nextInt();
-        System.out.print("Informe o número de colunas: ");
+        System.out.print("Nª de colunas: "); // Informando o número de colunas de uma matriz.
         int m = scanner.nextInt();
 
-        int[][] num = new int[m][n];
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m;j++ ){
-                System.out.println("Informe um número:  ");
-                num[i][m] = scanner.nextInt();
+        int[][] matrix = new int[n][m]; // Declarando e inicializando uma matriz.
 
+        System.out.println("Informe os números:  "); // Informando os valores contidos numa matriz.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = scanner.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++ ){
-                System.out.print([i][j]);
+        for (int i = 0; i < n; i++) { // Imprimindo os valores armazenados em formato de matriz.
+            System.out.println("\t");
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
         }
 
